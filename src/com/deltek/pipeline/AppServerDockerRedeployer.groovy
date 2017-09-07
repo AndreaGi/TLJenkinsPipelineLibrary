6645c35623dc955f087149a7f9341105c4f8187d
AndreaGi <andreaghetti@deltek.com>
2017-09-07 16:28:59 -0700

    TFC-13068 -Update of Pipeline functions

--- src/com/deltek/pipeline/AppServerDockerRedeployer.groovy
+++ src/com/deltek/pipeline/AppServerDockerRedeployer.groovy
@@ -2,19 +2,23 @@ package com.deltek.pipeline
 
 class AppServerDockerRedeployer implements Serializable{
 
+    def getShell() {
+        new Shell()
+    }
+
     public def redeploy(String ServerIP, String DockerImage, String CredentialsId){
         node('Redeploy') {
             stage("Redeploy $ServerIP"){
                 withCredentials([usernamePassword(credentialsId: "${CredentialsId}", usernameVariable: 'DOCKER_USR', passwordVariable: 'DOCKER_PWD' )]){
-                    sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker login -u $DOCKER_USR -p $DOCKER_PWD"
-                    sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker pull ${DockerImage}"
-                    sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker stop tomcat"
-                    sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \\" +
+                    getShell().pipe( "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker login -u $DOCKER_USR -p $DOCKER_PWD" )
+                    getShell().pipe( "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker pull ${DockerImage}" )
+                    getShell().pipe( "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker stop tomcat" )
+                    getShell().pipe( "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \\" +
                     "-v /logs:/logs \\ " +
                     "-p 8080:8080/tcp -p 22222:22222/tcp -p 22223:22223/tcp \\" +
-                    "--name=tomcat ${DockerImage}"
-                    sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker cp /docker/tomcat/conf/. tomcat:/usr/local/conf/"
-                    sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker start tomcat"
+                    "--name=tomcat ${DockerImage}" )
+                    getShell().pipe( "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker cp /docker/tomcat/conf/. tomcat:/usr/local/conf/" )
+                    getShell().pipe( "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker start tomcat" )
                 }
             }
         }
