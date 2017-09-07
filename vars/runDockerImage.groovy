1057c6900bd3b80340baf6a5938e891acc58e712
AndreaGi <andreaghetti@deltek.com>
2017-09-07 16:56:38 -0700

    TFC-13068 -Update of Pipeline functions

--- vars/runDockerImage.groovy
+++ vars/runDockerImage.groovy
@@ -4,10 +4,10 @@ def call() {
             sh "ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker login -u $DOCKER_USR -p $DOCKER_PWD"
             sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker pull ${config.DockerImage}"
             sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker stop tomcat"
-            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \
-                    -v /logs:/logs \
-                    -p 8080:8080/tcp -p 22222:22222/tcp -p 22223:22223/tcp \
-                    --name=tomcat ${config.DockerImage}"
+            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \\ " +
+                    "-v /logs:/logs \\ " +
+                    "-p 8080:8080/tcp -p 22222:22222/tcp -p 22223:22223/tcp \\ " +
+                    "--name=tomcat ${config.DockerImage}"
             sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker cp /docker/tomcat/conf/. tomcat:/usr/local/conf/"
             sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker start tomcat"
         }
