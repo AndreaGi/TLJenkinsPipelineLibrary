7f5d5b828bd07b1d6359855ed182ec6d2bdbfe1e
AndreaGi <andreaghetti@deltek.com>
2017-09-07 17:04:09 -0700

    TFC-13068 -Update of Pipeline functions

--- vars/runDockerImage.groovy
+++ vars/runDockerImage.groovy
@@ -1,15 +1,13 @@
-def call() {
-    node{
-        withCredentials([usernamePassword(credentialsId: "${config.CredentialsId}", usernameVariable: 'DOCKER_USR', passwordVariable: 'DOCKER_PWD')]) {
-            sh "ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker login -u $DOCKER_USR -p $DOCKER_PWD"
-            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker pull ${config.DockerImage}"
-            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker stop tomcat"
-            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \\ " +
-                    "-v /logs:/logs \\ " +
-                    "-p 8080:8080/tcp -p 22222:22222/tcp -p 22223:22223/tcp \\ " +
-                    "--name=tomcat ${config.DockerImage}"
-            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker cp /docker/tomcat/conf/. tomcat:/usr/local/conf/"
-            sh"ssh -o StrictHostKeyChecking=no jenkins@${config.ServerIP} sudo docker start tomcat"
-        }
+def call(String ServerIp, String CredentialsId, String DockerImage) {
+    withCredentials([usernamePassword(credentialsId: "${CredentialsId}", usernameVariable: 'DOCKER_USR', passwordVariable: 'DOCKER_PWD')]) {
+        sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker login -u $DOCKER_USR -p $DOCKER_PWD"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker pull ${DockerImage}"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker stop tomcat"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \\ " +
+                "-v /logs:/logs \\ " +
+                "-p 8080:8080/tcp -p 22222:22222/tcp -p 22223:22223/tcp \\ " +
+                "--name=tomcat ${DockerImage}"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker cp /docker/tomcat/conf/. tomcat:/usr/local/conf/"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker start tomcat"
     }
 }
