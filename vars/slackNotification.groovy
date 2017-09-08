bc7e8d5d38d9a98eac10268c301f4b730ffee9c6
AndreaGi <andreaghetti@deltek.com>
2017-09-08 11:38:57 -0700

    TFC-13068 - Updated SlackNotification with correct Jenkins string result

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,15 +1,14 @@
-def call(String buildStatus = 'SUCCESSFUL', Long duration) {
+def call(String buildStatus = 'SUCCESS', Long duration) {
     // build status of null means successful
-    buildStatus =  buildStatus ?: 'SUCCESSFUL'
+    buildStatus =  buildStatus ?: 'SUCCESS'
 
     // Default values
-    def colorName = 'RED'
     def colorCode = '#d00000'
     def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
     def summary = "${subject} (${env.BUILD_URL})"
 
     // Override default values based on build status
-    if (buildStatus == 'SUCCESSFUL') {
+    if (buildStatus == 'SUCCESS') {
         color = 'GREEN'
         colorCode = '#36a64f'
     } else {
