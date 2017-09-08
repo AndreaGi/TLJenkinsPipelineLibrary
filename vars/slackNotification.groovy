f6b3bda4b01c211d25882fb1a350ed639894e6a9
AndreaGi <andreaghetti@deltek.com>
2017-09-08 12:32:47 -0700

    TFC-13068 - Updated Slack notification - moved date format from scrip to function

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,10 +1,12 @@
-def call(String buildStatus = 'SUCCESS', String duration) {
+def call(String buildStatus = 'SUCCESS', Long duration) {
     // build status of null means successful
     buildStatus =  buildStatus ?: 'SUCCESS'
+    def slackNotificationFormat = new SimpleDateFormat("'after' m 'min' s 'sec'")
+    def durationString = slackNotificationFormat.format(new Date(duration))
 
     // Default values
     def colorCode = '#d00000'
-    def subject = "${env.JOB_NAME} - #${env.BUILD_NUMBER} ${buildStatus} after ${duration}"
+    def subject = "${env.JOB_NAME} - #${env.BUILD_NUMBER} ${buildStatus} after ${durationString}"
     def summary = "${subject} (${env.BUILD_URL})"
 
     // Override default values based on build status
