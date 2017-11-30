e2805a9bc10d7cb90fcb26f1758a4b1829d124ca
AndreaGi <andreaghetti@deltek.com>
2017-11-30 17:06:59 +0100

    TFC-13139 - Fixing wrong url on docker command on dev and test redeploy

--- vars/runTrafficLiveDockerImage.groovy
+++ vars/runTrafficLiveDockerImage.groovy
@@ -3,7 +3,8 @@ def call(String ServerIP, String CredentialsId, String DockerImage) {
         sh "ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker login -u $DOCKER_USR -p $DOCKER_PWD"
         sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker pull ${DockerImage}"
         // If the container is already stopped/not existing we return true anyway to not let the job fails
-        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker stop tomcat || true"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo systemctl stop docker-tomcat.service || true"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker rm tomcat || true"
         sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker create --rm --env-file /docker/tomcat/conf/tomcat.conf \\ " +
                 "-v /logs:/logs \\ " +
                 "-p 8080:8080/tcp -p 22222:22222/tcp -p 22223:22223/tcp \\ " +
