d4dc343c9da936e95f7d3d553a9844cb80e9fbad
AndreaGi <andreaghetti@deltek.com>
2018-01-26 17:30:01 +0100

    TFC-13163 - Updated function to work seamless as the puppet script

--- vars/runTrafficLiveDockerImage.groovy
+++ vars/runTrafficLiveDockerImage.groovy
@@ -7,9 +7,9 @@ def call(String ServerIP, String CredentialsId, String DockerImage, String Image
         sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo systemctl stop docker-tomcat.service || true"
         sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker stop tomcat || true"
         sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker rm tomcat || true"
-        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} docker build -t trafficlive/tomcat:${ImageTag} /docker/tomcat"
-        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sed -i 's|trafficlive/tomcat:.*|trafficlive/tomcat:${ImageTag}|g' /etc/systemd/system/docker-tomcat.service"
-        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} systemctl daemon-reload"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo docker build -t trafficlive/tomcat:${ImageTag} /docker/tomcat"
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo \"sed -i 's|trafficlive/tomcat:.*|trafficlive/tomcat:${ImageTag}|g' /etc/systemd/system/docker-tomcat.service \" "
+        sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo systemctl daemon-reload"
         sh"ssh -o StrictHostKeyChecking=no jenkins@${ServerIP} sudo systemctl start docker-tomcat.service "
     }
 }
